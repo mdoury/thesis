00c70c28d4af1c2ac446d55185f95ab19eabad15
mdoury <maxime.doury@lib.upmc.fr>
2017-04-26 11:02:19 +0200

    Test for .gitignore

--- compilationscript.bash
+++ compilationscript.bash
@@ -2,6 +2,7 @@ pdflatex main
 for i in `ls *.aux`; do bibtex $i; done
 pdflatex main
 pdflatex main
-
+# rm *.aux *.bbl *.blg *.out
+# rm main.{brf,idx,lof,log,lot,toc}
 
 open ./main.pdf -a Preview
